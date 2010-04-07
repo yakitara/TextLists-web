8633ee11230e547a0e2c3ce7adefa15d74021213
hiroshi <hiroshi@hiroshi-macbook.local>
2010-04-07 23:54:12 +0900

    initial commit with blueprint.css

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '8532c83c7c22097411093ef17fef28a181d885f9d012800ffc7e35707e69c14f65acf79e798fae80f003512193c9c4cbb88da248c5d726ad0b4c4301e2beb83f'
