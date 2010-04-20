c0ee8e9b26df265e82aef9179cd53b4ee8d82fde
hiroshi <hiroshi@hiroshi-macbook.local>
2010-04-20 11:54:12 +0900

    Use PostgreSQL instead of Sqlite3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter do
-    @lists = List.joins("LEFT JOIN listings ON listings.list_id = lists.id").group("lists.id")
-    @lists = @lists.order("lists.position ASC").all(:select => "lists.*, COUNT(listings.id) AS item_count")
+    selects = "lists.id, lists.name, lists.position" # for PostgreSQL
+    @lists = List.joins("LEFT JOIN listings ON listings.list_id = lists.id").group(selects)
+    @lists = @lists.order("lists.position ASC").all(:select => "#{selects}, COUNT(listings.id) AS item_count")
   end
 end
