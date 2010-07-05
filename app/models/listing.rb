e839c80b2b9034a107ac7927a5a7793f41a12d28
Hiroshi Saito <hiroshi3110@gmail.com>
2010-07-06 01:50:09 +0900

    Added Listing.without_default_scope to cancel default_scope :deleted_at = NULL

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,8 @@
 class Listing < ActiveRecord::Base
   default_scope :conditions => {:deleted_at => nil}, :order => "position ASC, created_at DESC"
+  def self.without_default_scope(&block)
+    self.with_exclusive_scope(&block)
+  end
   belongs_to :list, :inverse_of => :listings
   belongs_to :item, :inverse_of => :listings
   belongs_to :user, :inverse_of => :listings
