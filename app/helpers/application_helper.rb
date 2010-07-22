102f3672145a1070cee4992d400fe18e33efa158
Hiroshi Saito <hiroshi3110@gmail.com>
2010-07-22 09:11:49 +0900

    Extract external links of items as external link icons at items#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def extract_external_links(text)
+    text.scan(ActionView::Helpers::TextHelper::AUTO_LINK_RE).map do
+      link_to(image_tag("link.png"), $&, :target => "_blank")
+    end.join.html_safe
+  end
 end
---
+++ public/images/README.txt
@@ -0,0 +1,2 @@
+link.png is copied from:
+  http://commons.wikimedia.org/wiki/File:Icon_External_Link.png
